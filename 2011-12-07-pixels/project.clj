80f0548d7fb7aa2e104c5c6ff087a48d715613d0
Cédric Pineau <cedric.pineau@gmail.com>
2011-12-07 13:56:59 +0100

    Dojo du 7 décembre : Remplacement d'une couleur par une autre dans une image

---
+++ 2011-12-07-pixels/project.clj
@@ -0,0 +1,11 @@
+
+(defproject template "0.0.1-SNAPSHOT"
+  :description "Kata"
+  :dependencies [[clojure "1.2.0"]
+                 [org.clojure/tools.logging "0.1.2"]
+                 [log4j "1.2.16"]
+                 [midje "1.2.0"]]
+  :dev-dependencies [[lein-midje "1.0.5"]
+                     [com.intelie/lazytest "1.0.0-SNAPSHOT"]]
+)
+
